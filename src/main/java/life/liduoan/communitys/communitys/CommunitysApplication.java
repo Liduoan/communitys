3d5f49bc7290b0ba9fe6a2ca696ff31b1ed09103
Liduoan <1093762409@qq.com>
2021-02-10 17:06:16 +0800

    src come in

---
+++ src/main/java/life/liduoan/communitys/communitys/CommunitysApplication.java
@@ -0,0 +1,14 @@
+package life.liduoan.communitys.communitys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitysApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CommunitysApplication.class, args);
+	}
+
+}
